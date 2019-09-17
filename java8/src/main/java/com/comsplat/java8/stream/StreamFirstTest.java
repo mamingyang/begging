ebec380d47b3579b9ac872338c7b2fd3aa80c48e
Mamy <mamingyang@haier.com>
2019-09-17 10:21:28 +0800

    初始化

---
+++ java8/src/main/java/com/comsplat/java8/stream/StreamFirstTest.java
@@ -0,0 +1,17 @@
+package com.comsplat.java8.stream;
+
+/**
+ * @program: begging
+ * @description: Stream学习
+ * @author: Mamy
+ * @create: 2019-09-17 10:16
+ */
+public class StreamFirstTest {
+
+
+    public static void main(String[] args) {
+        System.out.println("1");
+    }
+
+
+}
